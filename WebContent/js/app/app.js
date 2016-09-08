f15893ba10cf1bcb8a0bb7154b0042a393023d0a
LRegis <leonardocregis@gmail.com>
2016-09-08 16:12:36 -0300

    adiction of the default value for routes

--- WebContent/js/app/app.js
+++ WebContent/js/app/app.js
@@ -2,8 +2,12 @@ var aplicativo = angular.module('myapp',['minhasDiretivas','ngAnimate','ngRoute'
 .config(function($routeProvider){
 	
 	$locationProvider.html5Mode(true);
+
     $routeProvider.when('/fotos', {
         templateUrl: 'partials/principal.html',
         controller: 'minhasDiretivas'
     });
+    
+
+    $routeProvider.otherwise({redirectTo: '/fotos'});e
 });
