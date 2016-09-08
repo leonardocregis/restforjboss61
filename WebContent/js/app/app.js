081046f15b4994c0d452cc6ae7ae0c7b1cff8a97
LRegis <leonardocregis@gmail.com>
2016-09-08 15:10:50 -0300

    adiciton of the route

--- WebContent/js/app/app.js
+++ WebContent/js/app/app.js
@@ -1,1 +1,9 @@
-var aplicativo = angular.module('myapp',['minhasDiretivas','ngAnimate']);
+var aplicativo = angular.module('myapp',['minhasDiretivas','ngAnimate','ngRoute'])
+.config(function($routeProvider){
+	
+	$locationProvider.html5Mode(true);
+    $routeProvider.when('/fotos', {
+        templateUrl: 'partials/principal.html',
+        controller: 'minhasDiretivas'
+    });
+});
