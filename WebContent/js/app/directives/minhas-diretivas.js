5ebac0f374ca5767d95df6c46986d0f349c21696
LRegis <leonardocregis@gmail.com>
2016-09-09 15:02:39 -0300

    adicao de nova diretiva para separar as fotos

--- WebContent/js/app/directives/minhas-diretivas.js
+++ WebContent/js/app/directives/minhas-diretivas.js
@@ -2,12 +2,21 @@
  * 
  */
 
-angular.module('minhasDiretivas',[]).directive('meuParagrafo',function (){
+var myDirectives = angular.module('minhasDiretivas',[]).directive('meuPainel',function (){
 	
 	var ddo = {};
 	ddo.restrict= 'AE';
 	ddo.scope = { titulo : '@'};
 	ddo.transclude=true;
 	ddo.templateUrl = 'js/app/directives/meu-painel.html';
+	return ddo;
+});
+
+myDirectives.directive('minhaFoto',function (){
+	
+	var ddo = {};
+	ddo.restrict= 'AE';
+	ddo.scope = { titulo : '@',url:'@'};
+	ddo.templateUrl = 'js/app/directives/minha-foto.html';
 	return ddo;
 });
