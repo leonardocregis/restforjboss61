07b56460ccfc4425d496a8b5cae2a31140f95732
LRegis <leonardocregis@gmail.com>
2016-09-14 14:26:55 -0300

    adiction of the message $scope

--- WebContent/js/app/ImageCtr.js
+++ WebContent/js/app/ImageCtr.js
@@ -4,9 +4,12 @@
 
 angular.module('myapp').controller('ImageCtr',['$scope','$http',function($scope,$http){
 		$scope.foto = {};
+		$scope.message = '';
+		
 		$scope.submit = function submit(){
 			if ($scope.formulario.$valid){
-				console.log('submitting');
+				console.log('submited');
+				$scope.message='submited';
 			}
 		};
 		
