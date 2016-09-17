978bfb57fe39fc6c1cdc7502df5755df8496e0a7
Leonardocregis <leonardocregis@gmail.com>
2016-09-17 13:37:02 -0300

    implementation of the http post with message treatment

--- WebContent/js/app/ImageCtr.js
+++ WebContent/js/app/ImageCtr.js
@@ -8,10 +8,16 @@ angular.module('myapp').controller('ImageCtr',['$scope','$http',function($scope,
 		
 		$scope.submit = function submit(){
 			if ($scope.formulario.$valid){
-				console.log('submited');
-				$scope.foto= {};
-				$scope.message='Imagem cadastrada com sucesso';
-			}
+				   $http.post('rest/posts', $scope.foto)
+	                .success(function() {
+	                	$scope.foto={};
+	                    $scope.mensagem = 'Foto cadastrada com sucesso';
+	                })
+	                .error(function(erro) {
+	                    console.log(erro);
+	                    $scope.mensagem = 'Não foi possível cadastrar a foto';
+	                });	
+		    }
 		};
 		
 }]);
