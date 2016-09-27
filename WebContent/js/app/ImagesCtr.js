592d356ae475b333d1890a3102f24cf4fadb6497
Leonardocregis <leonardocregis@gmail.com>
2016-09-26 21:00:52 -0300

    aded remove functio and delete $http function

--- WebContent/js/app/ImagesCtr.js
+++ WebContent/js/app/ImagesCtr.js
@@ -14,5 +14,19 @@ angular.module('myapp').controller('ImagesCtr',['$scope','$http',function($scope
 			.error(function(error){
 				console.log(error);
 			});
-		
+
+		$scope.remover = function remover(foto){
+			var idFoto = $scope.fotos.lastIndexOf(foto);
+			console.log(idFoto);
+			$http.delete('rest/posts/'+idFoto)
+			.success(function(retorno){
+				console.log("sucess")
+				$scope.message = retorno;
+			})
+			.error(function(error){
+				console.log("failure");
+				$scope.message(error);
+			});
+			
+		}
 }]);
