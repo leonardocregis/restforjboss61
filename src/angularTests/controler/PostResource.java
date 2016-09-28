aaa6b9796012415c11dc271b8ca0b6e3d90e4016
LRegis <leonardocregis@gmail.com>
2016-09-28 16:28:56 -0300

    adiction of update method

--- src/angularTests/controler/PostResource.java
+++ src/angularTests/controler/PostResource.java
@@ -6,6 +6,7 @@ import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -38,6 +39,15 @@ public class PostResource {
 	@Produces(MediaType.APPLICATION_JSON)
 	public Post getPost(@PathParam("id")String id){
 		return dbAngular.get(id);
+	}
+
+
+	@PUT
+	@Path("{id}")
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_JSON)
+	public void updatePost(@PathParam("id")int id,Post post){
+		dbAngular.set(id,post);
 	}
 	
 	@POST
